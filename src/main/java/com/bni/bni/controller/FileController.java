a62774ff3808d6acafd7fa6d70fc4edcef82ee3f
kautsarakasyah <kautsarakasyah2gmail.com>
2025-06-10 14:37:04 +0700

    updated

--- src/main/java/com/bni/bni/controller/FileController.java
+++ src/main/java/com/bni/bni/controller/FileController.java
@@ -1,59 +1,76 @@
  package com.bni.bni.controller;
 
-
-import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/files")
 public class FileController {
-
     @Value("${file.upload-dir}")
     private String uploadDir;
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<Map<String, Object>> upload(@RequestParam("file") MultipartFile file) {
+
         try {
-            // Logic to save the file to the upload directory
-            Path uploadPath = Paths.get(uploadDir);
+            final var uploadPath = Paths.get(uploadDir);
             if (!Files.exists(uploadPath)) {
                 Files.createDirectories(uploadPath);
             }
-
-            String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-            Path filePath = uploadPath.resolve(fileName);
+            final var filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            final var filePath = uploadPath.resolve(filename).normalize();
             Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+            final var fileUrl = "/api/files/" + filename;
 
-            String fileUrl = "/api/files/" + fileName;
-
-            return ResponseEntity.ok().body(
-                    Map.of(
-                           "status", 200,
-                        "message", "File uploaded successfully",
-                        "fileName", fileName,
-                        "fileUrl", fileUrl
-            )
-            );
-        } catch (IOException e) {
-            return ResponseEntity.status(500).body(
-                    Map.of(
-                            "status", 500,
-                            "message", "Failed to upload file: " + e.getMessage()
-                    )
-            );
+            return ResponseEntity.ok(Map.of(
+                    "status", 200,
+                    "message", "File uploaded successfully",
+                    "filename", filename,
+                    "fileUrl", fileUrl
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(Map.of(
+                    "status", 500,
+                    "message", "File upload failed: " + e.getMessage()
+            ));
         }
     }
 
+    @GetMapping("/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        try {
+            final var filePath = Paths.get(uploadDir).resolve(filename).normalize();
+            final var resource = new UrlResource(filePath.toUri());
+            if (!resource.exists()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            var contentType = Files.probeContentType(filePath);
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION,"inline; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (MalformedURLException exception) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
 }
